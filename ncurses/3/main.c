9b441bc7f8f1f3788b98689aaf0307ee73061a7b
Rafał <example@invalid.com>
2023-04-04 22:11:50 +0200

    ncruses jazda

---
+++ ncurses/3/main.c
@@ -0,0 +1,17 @@
+#include <ncurses.h>
+#include <string.h>
+
+int main() {
+  char mesg[] = "Ener a string: ";
+  char str[80];
+  int row, col;
+  initscr();
+  getmaxyx(stdscr, row, col);
+  mvprintw(row / 2, (col - strlen(mesg)) / 2, "%s", mesg);
+
+  getstr(str);
+  mvprintw(LINES - 2, 0, "You entered: %s", str);
+  getch();
+  endwin();
+  return 0;
+}
