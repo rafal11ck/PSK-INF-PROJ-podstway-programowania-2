f2c00cbc69a14a17204ff45b47a8c54f075d3d48
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2023-04-06 13:03:41 +0200

    klienci struktura

---
+++ src/klient.h
@@ -0,0 +1,13 @@
+#ifndef KLIENT_H_
+#define KLIENT_H_
+
+/**
+ * @brief Structure hodling client data
+ * */
+struct Client {
+  int m_id;        //! Unique id
+  char *m_name;    //! String holding first name
+  char *m_surname; //! String holding second name
+}
+
+#endif // KLIENT_H_
