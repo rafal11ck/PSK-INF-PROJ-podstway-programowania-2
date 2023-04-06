5c435ec977a7e839d8a87e9a1259df1472a4c8df
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2023-04-06 13:30:19 +0200

    klient
    klientów (numer karty, imię, nazwisko, adres, telefon)

--- src/klient.h
+++ src/klient.h
@@ -5,9 +5,16 @@
  * @brief Structure hodling client data
  * */
 struct Client {
-  int m_id;        //! Unique id
-  char *m_name;    //! String holding first name
-  char *m_surname; //! String holding second name
+  //! Unique card ID
+  int m_cardId;
+  //! String holding first name
+  char *m_name;
+  //! String holding second name
+  char *m_surname;
+  //! Adress
+  char *m_adress;
+  //! phone number
+  int m_phone;
 };
 
 #endif // KLIENT_H_
