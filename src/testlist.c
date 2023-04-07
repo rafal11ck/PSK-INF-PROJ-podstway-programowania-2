1f0d517f32fb6a725f184ae585dd03287f9fa43c
Rafał <example@invalid.com>
2023-04-07 21:02:52 +0200

    testlist

--- src/testlist.c
+++ src/testlist.c
@@ -15,13 +15,29 @@
 bool intCmp(const void *a, const void *b) { return *(int *)(a) < *(int *)(b); }
 
 /**
+ *@brief Prints gien List to standard output.
+ *@param list List to be printed pointer.
+ *@returns false
+ **/
+bool printList(struct List *list) {
+  printf("\nlist=\t");
+  struct ListNode *it = listGetFront(list);
+  while (it != NULL) {
+    printf("%d\t", *(int *)(it->m_data));
+    it = it->m_next;
+  }
+  printf("\n");
+  return false;
+}
+
+/**
  * @brief Runs unit test of list.
  * */
 int main() {
   printf("Create Empty list...");
   struct List *list = NULL;
   list = listCreateList();
-  puts("PASS");
+  printList(list);
 
   printf("Add elements the front of list...");
   for (int i = 1; i < N + 1; ++i) {
@@ -29,7 +45,7 @@ int main() {
     *data = i;
     listPushFront(list, data);
   }
-  puts("PASS");
+  printList(list);
 
   printf("Add elements the back of list...");
   for (int i = 20; i < N + 21; ++i) {
@@ -37,9 +53,12 @@ int main() {
     *data = i;
     listPushBack(list, data);
   }
-  puts("PASS");
+  printList(list);
 
-  printf("Insert element at front of list...");
-  listInsert(list, list->m_front, &intCmp);
-  puts("PASS");
+  printf("Insert given node...");
+  struct ListNode *it = listGetFront(list);
+  int *x = malloc(sizeof(int));
+  *x = 3;
+  listInsert(list, x, &intCmp);
+  printList(list);
 }
