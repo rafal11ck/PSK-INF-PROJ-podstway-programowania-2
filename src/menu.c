e5e5fb08edcc7a805e757a0f67ed9da4adf5c6b7
Rafał <example@invalid.com>
2023-04-14 21:22:54 +0200

    Revert "Naprawa Windows momentu i kijowego kodowania znakow"
    This reverts commit 4328599a03d1c87bc79b4e1d14ca61f924ff9d73.

--- src/menu.c
+++ src/menu.c
@@ -22,7 +22,7 @@ void mainMenu() {
       system("cls");
       printf("Main Menu:\n");
 
-      // WyÅ›wietl opcje menu
+      // Wyœwietl opcje menu
       for(i = 0; i < liczba_opcji; i++) {
          if(i == wybor) {
             printf(" -> %s\n", opcje[i]);
@@ -32,15 +32,15 @@ void mainMenu() {
          }
       }
 
-      // Odczytaj naciÅ›niÄ™ty klawisz
+      // Odczytaj naciœniêty klawisz
       int klawisz = getch();
 
       if(klawisz == 224) {
          klawisz = getch();
-         if(klawisz == 72 && wybor > 0) { // StrzaÅ‚ka w gÃ³rÄ™
+         if(klawisz == 72 && wybor > 0) { // Strza³ka w górê
             wybor--;
          }
-         else if(klawisz == 80 && wybor < liczba_opcji - 1) { // StrzaÅ‚ka w dÃ³Å‚
+         else if(klawisz == 80 && wybor < liczba_opcji - 1) { // Strza³ka w dó³
             wybor++;
          }
       }
@@ -60,13 +60,13 @@ void mainMenu() {
                break;
          }
 
-         // ZakoÅ„cz program, jeÅ›li uÅ¼ytkownik wybraÅ‚ opcjÄ™ "WyjÅ›cie"
+         // Zakoñcz program, jeœli u¿ytkownik wybra³ opcjê "Wyjœcie"
          if(wybor == liczba_opcji - 1) {
             break;
          }
 
-         // Poczekaj na naciÅ›niÄ™cie dowolnego klawisza, aby wrÃ³ciÄ‡ do menu
-         printf("NaciÅ›nij dowolny klawisz, aby kontynuowaÄ‡...");
+         // Poczekaj na naciœniêcie dowolnego klawisza, aby wróciæ do menu
+         printf("Naciœnij dowolny klawisz, aby kontynuowaæ...");
          getch();
       }
    } while(1);
@@ -82,7 +82,7 @@ void carsMenu() {
       system("cls");
       printf("Cars Menu:\n");
 
-      // WyÅ›wietl opcje menu
+      // Wyœwietl opcje menu
       for(i = 0; i < liczba_opcji; i++) {
          if(i == wybor) {
             printf(" -> %s\n", opcje[i]);
@@ -92,15 +92,15 @@ void carsMenu() {
          }
       }
 
-      // Odczytaj naciÅ›niÄ™ty klawisz
+      // Odczytaj naciœniêty klawisz
       int klawisz = getch();
 
       if(klawisz == 224) {
          klawisz = getch();
-         if(klawisz == 72 && wybor > 0) { // StrzaÅ‚ka w gÃ³rÄ™
+         if(klawisz == 72 && wybor > 0) { // Strza³ka w górê
             wybor--;
          }
-         else if(klawisz == 80 && wybor < liczba_opcji - 1) { // StrzaÅ‚ka w dÃ³Å‚
+         else if(klawisz == 80 && wybor < liczba_opcji - 1) { // Strza³ka w dó³
             wybor++;
          }
       }
@@ -125,13 +125,13 @@ void carsMenu() {
                break;
          }
 
-         // ZakoÅ„cz program, jeÅ›li uÅ¼ytkownik wybraÅ‚ opcjÄ™ "WyjÅ›cie"
+         // Zakoñcz program, jeœli u¿ytkownik wybra³ opcjê "Wyjœcie"
          if(wybor == liczba_opcji - 1) {
             break;
          }
 
-         // Poczekaj na naciÅ›niÄ™cie dowolnego klawisza, aby wrÃ³ciÄ‡ do menu
-         printf("NaciÅ›nij dowolny klawisz, aby kontynuowaÄ‡...");
+         // Poczekaj na naciœniêcie dowolnego klawisza, aby wróciæ do menu
+         printf("Naciœnij dowolny klawisz, aby kontynuowaæ...");
          getch();
       }
    } while(1);
@@ -147,7 +147,7 @@ void clientsMenu() {
       system("cls");
       printf("Clients Menu:\n");
 
-      // WyÅ›wietl opcje menu
+      // Wyœwietl opcje menu
       for(i = 0; i < liczba_opcji; i++) {
          if(i == wybor) {
             printf(" -> %s\n", opcje[i]);
@@ -157,9 +157,103 @@ void clientsMenu() {
          }
       }
 
-      // Odczytaj naciÅ›niÄ™ty klawisz
+      // Odczytaj naciœniêty klawisz
       int klawisz = getch();
 
       if(klawisz == 224) {
          klawisz = getch();
-         if(kla
+         if(klawisz == 72 && wybor > 0) { // Strza³ka w górê
+            wybor--;
+         }
+         else if(klawisz == 80 && wybor < liczba_opcji - 1) { // Strza³ka w dó³
+            wybor++;
+         }
+      }
+      else if(klawisz == 13) { // ENTER
+         switch(wybor) {
+            case 0:
+               printf("Wybrales opcje: listClients\n");
+               break;
+            case 1:
+               printf("Wybrales opcje: addClients\n");
+               break;
+            case 2:
+               printf("Wybrales opcje: removeClients\n");
+               break;
+            case 3:
+               printf("Wybrales opcje: editClients\n");
+               break;
+            case 4:
+               break;
+         }
+
+         // Zakoñcz program, jeœli u¿ytkownik wybra³ opcjê "Wyjœcie"
+         if(wybor == liczba_opcji - 1) {
+            break;
+         }
+
+         // Poczekaj na naciœniêcie dowolnego klawisza, aby wróciæ do menu
+         printf("Naciœnij dowolny klawisz, aby kontynuowaæ...");
+         getch();
+      }
+   } while(1);
+}
+
+void rentalsMenu() {
+       int wybor = 0;
+   char *opcje[] = {"listRents", "addRent", "returnRent", "returnToMainMenu"};
+   int liczba_opcji = sizeof(opcje)/sizeof(opcje[0]);
+   int i;
+
+   do {
+      system("cls");
+      printf("Rents Menu:\n");
+
+      // Wyœwietl opcje menu
+      for(i = 0; i < liczba_opcji; i++) {
+         if(i == wybor) {
+            printf(" -> %s\n", opcje[i]);
+         }
+         else {
+            printf("    %s\n", opcje[i]);
+         }
+      }
+
+      // Odczytaj naciœniêty klawisz
+      int klawisz = getch();
+
+      if(klawisz == 224) {
+         klawisz = getch();
+         if(klawisz == 72 && wybor > 0) { // Strza³ka w górê
+            wybor--;
+         }
+         else if(klawisz == 80 && wybor < liczba_opcji - 1) { // Strza³ka w dó³
+            wybor++;
+         }
+      }
+      else if(klawisz == 13) { // ENTER
+         switch(wybor) {
+            case 0:
+               printf("Wybrales opcje: listRents\n");
+               break;
+            case 1:
+               printf("Wybrales opcje: addRent\n");
+               break;
+            case 2:
+               printf("Wybrales opcje: returnRent\n");
+               break;
+            case 3:
+               break;
+         }
+
+         // Zakoñcz program, jeœli u¿ytkownik wybra³ opcjê "Wyjœcie"
+         if(wybor == liczba_opcji - 1) {
+            break;
+         }
+
+         // Poczekaj na naciœniêcie dowolnego klawisza, aby wróciæ do menu
+         printf("Naciœnij dowolny klawisz, aby kontynuowaæ...");
+         getch();
+      }
+   } while(1);
+}
